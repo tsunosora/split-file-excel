0d025d8379814c9d1a3eea3c9c0f882b357b8120
tsunosora <muhamadfaisal288@gmail.com>
2024-11-15 13:16:53 +0700

    menambah fitur file picker

--- splitexcel.py
+++ splitexcel.py
@@ -1,13 +1,24 @@
 import pandas as pd
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
-# Ganti file_path sesuai dengan lokasi file CSV Anda
-file_path = r'D:\WA BLAST\UPDATE TOOL 28-08-24\HASIL SCRAPTING\Perusahaan Konstruksi\extracted_Perusahaan Konstruksi.csv'
-data = pd.read_csv(file_path)
+# Menampilkan file picker untuk memilih file CSV
+Tk().withdraw()  # Menyembunyikan jendela utama Tkinter
+file_path = askopenfilename(title="Pilih file CSV", filetypes=[("CSV Files", "*.csv")])
 
-# Tentukan jumlah data per file
-data_per_file = 200
+# Pastikan file dipilih
+if file_path:
+    # Membaca file CSV yang dipilih
+    data = pd.read_csv(file_path)
 
-# Loop untuk membagi data menjadi beberapa file
-for i in range(0, len(data), data_per_file):
-    subset_data = data[i:i+data_per_file]
-    subset_data.to_csv(f'customer_data_part_{i//data_per_file + 1}.csv', index=False)
+    # Tentukan jumlah data per file
+    data_per_file = 200
+
+    # Loop untuk membagi data menjadi beberapa file
+    for i in range(0, len(data), data_per_file):
+        subset_data = data[i:i+data_per_file]
+        subset_data.to_csv(f'customer_data_part_{i//data_per_file + 1}.csv', index=False)
+
+    print(f'File berhasil dibagi menjadi beberapa bagian.')
+else:
+    print("Tidak ada file yang dipilih.")
